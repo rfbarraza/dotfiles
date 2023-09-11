8ac2419808fcf5bedcef9d6162b677bacb1e7aa8
Rene Barraza <rfbarraza@gmail.com>
2023-09-11 14:10:46 -0600

    Fixing email task for wysd (sake)

--- .local/lib/zsh/sake-wysd-status.sh
+++ .local/lib/zsh/sake-wysd-status.sh
@@ -5,12 +5,19 @@
     sake -c ~/.config/sake/wysd/sake.yaml run ping;
     sake -c ~/.config/sake/wysd/sake.yaml run info;
     echo "\n\n"
+    echo "# ###################################################################"
     echo "# Docker"
     echo "# ###################################################################\n"
     sake -c ~/.config/sake/wysd/sake.yaml run docker-status;
     echo "\n\n"
+    echo "# ###################################################################"
     echo "# Call Attendant"
     echo "# ###################################################################\n"
     sake -c ~/.config/sake/wysd/sake.yaml run phone-status;
     sake -c ~/.config/sake/wysd/sake.yaml run phonecalls;
+    echo "\n\n"
+    echo "# ###################################################################"
+    echo "# E-Mail"
+    echo "# ###################################################################\n"
+    sake -c ~/.config/sake/wysd/sake.yaml run dads-email;
 } | sed 's/\[[0-9;]*m//g' | sed 's/\[0;1m//g' > ~/Desktop/sake-wysd-report.txt
