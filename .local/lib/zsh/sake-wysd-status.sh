80837b0e4d1c8959c8e12d9646a19c5c17ffdd44
Rene Barraza <rfbarraza@gmail.com>
2023-09-19 07:58:02 -0600

    Refactored sake and bouncer scripts to coincide with new subnetting schema

--- .local/lib/zsh/sake-wysd-status.sh
+++ .local/lib/zsh/sake-wysd-status.sh
@@ -2,23 +2,23 @@
 
 {
     sh_header -n "Wayside Report" -d "Server Status and Logs"
-    sake -c ~/.config/sake/wysd/sake.yaml run ping;
-    sake -c ~/.config/sake/wysd/sake.yaml run info;
+    sake -c ~/.config/sake/sake.yaml run wysd-ping;
+    sake -c ~/.config/sake/sake.yaml run wysd-info;
     echo "\n\n"
     echo "# ###################################################################"
     echo "# Docker"
     echo "# ###################################################################\n"
-    sake -c ~/.config/sake/wysd/sake.yaml run docker-status;
+    sake -c ~/.config/sake/sake.yaml run wysd-docker-status;
     echo "\n\n"
     echo "# ###################################################################"
     echo "# Call Attendant"
     echo "# ###################################################################\n"
-    sake -c ~/.config/sake/wysd/sake.yaml run phone-status;
-    sake -c ~/.config/sake/wysd/sake.yaml run phonecalls;
+    sake -c ~/.config/sake/sake.yaml run callattendant-status;
+    sake -c ~/.config/sake/sake.yaml run phonecalls;
     echo "\n\n"
     echo "# ###################################################################"
     echo "# E-Mail"
     echo "# ###################################################################\n"
-    sake -c ~/.config/sake/wysd/sake.yaml run dads-email;
-} | sed 's/\[[0-9;]*m//g' | sed 's/\[0;1m//g' > ~/Documents/Family/Wayside/Status/sake-report-$(date +%Y-%m-%d).txt
+    sake -c ~/.config/sake/sake.yaml run dads-email;
+} | sed 's/\[[0-9;]*m//g' | sed 's/\[0;1m//g' # > ~/Documents/Family/Wayside/Status/sake-report-$(date +%Y-%m-%d).txt
 
